cc90bf520bdb96aa9e2e9b81d4c9c37ab3fb706c
Ramon Ribeiro <rhpr@cesar.org.br>
2019-10-06 22:03:13 -0300

    Add express server

--- src/server/ExpressServer.js
+++ src/server/ExpressServer.js
@@ -1,11 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
 class ExpressServer {
-    constructor(userController) {
+    constructor(settings, userController) {
+        this.port = settings.port;
         this.userController = userController;
+        this.app = express();
     }
 
     start() {
-        console.log('Server started');
-        console.log(this.userController);
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.post('/user',
+                    this.userController.createUser.bind(this.userController));
+        this.app.listen(this.port, () => console.log('Server started'));
     }
 }
 
